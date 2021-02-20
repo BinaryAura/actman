521477c6ecdec0e06d9dac4ae02412ee91792e81
Johnathan Dunker <jad8w4@mst.edu>
2021-02-19 23:42:08 -0600

    Dealing with git

--- build/Debug/spdlog/fmt.d
+++
@@ -1,9 +0,0 @@
-../../build/Debug/spdlog/fmt.o: src/fmt.cpp \
-  include/spdlog/fmt/bundled/format-inl.h \
-  include/spdlog/fmt/bundled/format.h include/spdlog/fmt/bundled/core.h
-
-include/spdlog/fmt/bundled/format-inl.h:
-
-include/spdlog/fmt/bundled/format.h:
-
-include/spdlog/fmt/bundled/core.h:
