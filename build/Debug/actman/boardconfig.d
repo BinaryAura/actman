f1a499ca710ef1a4982172c3e67f581b1b063b29
Johnathan Dunker <jad8w4@mst.edu>
2021-02-19 23:40:54 -0600

    Dealing with git

---
+++ build/Debug/actman/boardconfig.d
@@ -0,0 +1,13 @@
+build/Debug/actman/boardconfig.o: src/boardconfig.cpp include/error.h \
+  include/board/boardconfig.h include/board/cell.h \
+  include/components/transform.h include/actmancommon.h
+
+include/error.h:
+
+include/board/boardconfig.h:
+
+include/board/cell.h:
+
+include/components/transform.h:
+
+include/actmancommon.h:
---
+++ build/Debug/spdlog/fmt.d
@@ -0,0 +1,9 @@
+../../build/Debug/spdlog/fmt.o: src/fmt.cpp \
+  include/spdlog/fmt/bundled/format-inl.h \
+  include/spdlog/fmt/bundled/format.h include/spdlog/fmt/bundled/core.h
+
+include/spdlog/fmt/bundled/format-inl.h:
+
+include/spdlog/fmt/bundled/format.h:
+
+include/spdlog/fmt/bundled/core.h:
